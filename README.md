b24347f4f1ef9ed7a48d93554ab5feffc6d457ad
josmuniz <jmuniz2011@gmail.com>
2023-01-25 15:38:19 -0500

    Upgrade project assignmet 1 - express

---
+++ README.md
@@ -0,0 +1,3 @@
+# COMP229 - M2023 - WEEK3D
+
+Project for COMP229 Assingment 1
--- lib.js
+++
@@ -1,8 +0,0 @@
-export function half0f(x){
-    return x/2;
-}
-
-export function multiply(x, y){
-    return x * y;
-}
-
