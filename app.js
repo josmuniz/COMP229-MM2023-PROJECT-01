b7ccdf30562e55baeaf453ea8de6a39d914cd1e9
josmuniz <jmuniz2011@gmail.com>
2023-02-01 15:27:58 -0500

    upgrade project assignemt1 final project

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -36,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error',{title: 'Error'});
+  res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
-});
-/* GET other pages. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'About' });
-});
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Products' });
-});
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Service' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
