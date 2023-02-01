83f183ad66592c48a67c26b6a3add1432e1f529a
josmuniz <jmuniz2011@gmail.com>
2023-01-31 21:54:20 -0500

    Upgrade3 project assignmet 1 - home

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+/* GET other pages. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Service' });
 });
 
 module.exports = router;
